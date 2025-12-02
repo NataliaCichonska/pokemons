73e5f0afe8e78f6bb4c69b2a39e4b70c0287feef
Natalia Cichońska <natalia@uniqsoft.pl>
2025-12-02 13:25:10 +0100

    delete basename

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/pokemons">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
